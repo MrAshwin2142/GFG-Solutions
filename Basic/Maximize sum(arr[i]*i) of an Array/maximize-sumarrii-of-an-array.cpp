b21fb86db070d123f7d75f61bb5a569ca90cec46
Ashwin Dhangar <89156541+MrAshwin2142@users.noreply.github.com>
2023-10-25 10:08:23 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Maximize sum(arr[i]*i) of an Array/maximize-sumarrii-of-an-array.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int Maximize(int a[],int n)
+    {
+        // Complete the function
+        long long int ans=0;
+        sort(a,a+n);
+        for(int i=0;i<n;i++){
+            ans+= 1ll * a[i] *i;
+        }
+        return ans %1000000007;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        cout<<ob.Maximize(a,n)<<endl;
+    }
+}
+// } Driver Code Ends
