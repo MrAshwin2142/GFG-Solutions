abdd5811747f42838c61bcc431cefcf9ed0e751a
Ashwin Dhangar <89156541+MrAshwin2142@users.noreply.github.com>
2023-10-26 13:47:54 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimum Operations/minimum-operations.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    int minOperation(int n)
+    {
+        //code here.
+        int ans=0;
+        while(n>0){
+            if(n%2==0){
+                n/=2;
+            }else n-=1;
+            ans++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	    {
+	        int n;
+	        cin>>n;
+	        Solution ob;
+	        cout<<ob.minOperation(n)<<endl;
+	    }
+}
+// } Driver Code Ends
